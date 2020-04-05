28293f468c4a5198b676c2c96b0ebb7d7691fadb
ShaoQiBNU <37075909+ShaoQiBNU@users.noreply.github.com>
2020-04-05 09:57:45 +0800

    update data and code
    update data and code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # COVID-19
 
-## COVID-19数据获取
+## COVID-19中国各行政级数据获取
 
 ```shell
 # 今日头条APP的疫情地图获取疫情数据：省级，地级市，区县级，输出 /data/COVID19_province.csv，/data/COVID19_city.csv，/data/COVID19_distinct.csv
@@ -8,4 +8,20 @@
 cd code
 
 python COVID19_from_TouTiao.py
+```
+
+#### 数据说明
+
+```shell
+中国2015年省级、地级市、区县级行政编码
+/data/china_location_id_2015.csv
+
+省级疫情数据
+/data/COVID19_province.csv   
+
+地级市疫情数据
+/data/COVID19_city.csv
+
+区县级疫情数据
+/data/COVID19_distinct.csv
 ```
---
+++ code/china_distinct_shp_COVID19.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# @Author  : Qi Shao
+
+import geopandas
+import pandas as pd
+import matplotlib.pyplot as plt
+
+path = '../shp/china_all_dissolve.shp'
+shp_df = geopandas.GeoDataFrame.from_file(path)
+shp_df.rename(columns={'PAC': 'id'}, inplace=True)
+
+china_distinct = pd.read_csv("../data/COVID19_distinct.csv", sep=',')
+china_distinct = china_distinct[['distinct_id', 'distinct_name', 'distinct_confirmedNum']]
+china_distinct.columns = ['id', 'name', 'confirmedNum']
+
+china_city = pd.read_csv("../data/COVID19_city.csv", sep=',')
+china_city = china_city[['city_id', 'city_name', 'city_confirmedNum']]
+china_city.columns = ['id', 'name', 'confirmedNum']
+
+
+china_province = pd.read_csv("../data/COVID19_province.csv", sep=',')
+china_province = china_province[['province_id', 'province_name', 'province_confirmedNum']]
+china_province.columns = ['id', 'name', 'confirmedNum']
+
+china = pd.concat([china_province, china_city, china_distinct])
+shp_df = pd.merge(shp_df, china, how='left', on='id')
+shp_df = shp_df.fillna(value=0)
+
+num = shp_df['confirmedNum'].to_list()
+
+level = [5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 90, 150, 250, 500, 1000, 3000, 6000, 9000]
+for i in range(1, len(level)):
+    res=0
+    for j in num:
+        if j<=level[i] and j>level[i-1]:
+            res+=1
+    print(level[i-1], level[i], res)
+
+
+shp_df.to_file("../shp/china_all_dissolve_COVID19.shp", encoding='utf-8')
+
+
+
+
+
+
+
+
+
+
+
